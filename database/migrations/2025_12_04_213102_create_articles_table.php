c9b3a35d3438dc252d0dc45cd44a721a7abe620b
kelvinmurimi <murimicodes@gmail.com>
2025-12-05 00:35:40 +0300

    add databse migration fields

--- database/migrations/2025_12_04_213102_create_articles_table.php
+++ database/migrations/2025_12_04_213102_create_articles_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('slug')->unique();
+            $table->foreignUid('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
+            //status: draft, published, archived
+            $table->enum('status', ['draft', 'published', 'archived'])->default('draft');
+            //excerpt
+            $table->text('excerpt')->nullable();
+            //featured image
+            $table->string('featured_image')->nullable();
             $table->timestamps();
         });
     }
